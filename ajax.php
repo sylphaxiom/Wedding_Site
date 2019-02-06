13daa813015f5ab7faced2558c6fb0cfdbb1de73
Jacob Pell <pelljacoba@gmail.com>
2019-02-05 20:55:52 -0500

    This is after a recent edit to the JS page and new creation of the ajax page for validation request.

---
+++ ajax.php
@@ -0,0 +1,14 @@
+<?php
+/*
+	Author: Jacob Pell
+	Date: 02/03/2019
+	Description: This page will serve as the ajax request page for the site
+	It will process all of the AJAX requests that are needed for the site 
+	to be able to validate and function properly.
+*/
+
+$dbName = "projectJP";
+
+require("connecti2db.inc.php");
+
+?>
--- wedding.js
+++ wedding.js
@@ -7,14 +7,59 @@ Description: This script will serve to validate the login form
 	     for validation and this is the best way to do the work.
 */
 
-function validateLogin() {
-  console.log("beginning login validation");
-  var uname = document.getElementById("UserName");
-  var pword = document.getElementById("Password");
-  var errSm = document.getElementById("error");
-  var unValid = true;
-  var pwValid = true;
-  try{
-    
-  }//end try
-}//end validateLogin
+function validateUname() {
+  	console.log("beginning username validation");
+  	var uname = document.getElementById("UserName");
+	var unValid = true;
+  	try{
+    	
+	}//end try
+}//end validateUname
+
+function AjaxFunction() {
+	console.log("Beginning AJAX request");
+	var ajaxRequest; // The variable that makes the AJAX magic possible!
+	try{
+		// REAL BROWSERS
+		ajaxRequest = new XMLHttpRequest();
+	} catch (e) {
+		// Internet Exploder Browsers
+		try{
+			ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try{
+				ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {
+				// something went wrong
+				alert("Your browser broke!");
+				return false;
+			}
+		}
+	}
+	
+	console.log("Now processing AJAX request");
+	ajaxRequest.onreadystatechange = function(){
+		console.log(ajaxRequest.statusText);
+		console.log(ajaxRequest.readyState);
+		if(ajaxRequest.readyState == 4) 
+		{
+			console.log("Entering IF READYSTATE");
+			var ajaxDisplay = document.getElementById('unameHelp');
+			var resultText = ajaxRequest.responseText;
+			ajaxDisplay.innerHTML = resultText;
+			console.log("response text is = " + resultText);
+		}
+	}
+	var uname = document.getElementById('username').value;
+	console.log("username is = " + username);
+	var queryString = "?uname=" + uname;
+	console.log("queryString is = " + queryString);
+	ajaxRequest.open("GET", "course-info-query.php" + queryString, true);
+	ajaxRequest.send(null);
+	console.log("Ending IF PROGRAM");
+}
+/*	This is where I'm putting bits I'll need later	*/
+/*
+	var pwValid = true;
+  	var pword = document.getElementById("Password");
+*/
