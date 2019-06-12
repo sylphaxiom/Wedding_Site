ca9e15f1d28f208934bf3d05f43dba726735f987
Jacob Pell <pelljacoba@gmail.com>
2019-06-12 01:07:26 -0400

    In process of creating DB logic to add user's address for directions button. Since it is not finished, construction message has been put up on the live site.

--- dateInfo.php
+++ dateInfo.php
@@ -31,7 +31,7 @@ require("weddingHead.inc");
 require("nav.inc");
 
 # Under construction notification for un-finished pages.
-/*
+
 echo <<<ARTDOC
 <article class="container text-center">
   <h2 class="col-md-6 offset-md-3 mx-auto">Oops!</h2>
@@ -39,8 +39,8 @@ echo <<<ARTDOC
   <img src="img/200px-Commons-emblem-Under_construction-green.svg.png" width="200" height="200" class="img-fluid col-md-3 mx-auto" alt="under construction img" />
 </article>
 ARTDOC;
-*/
 
+/*
 echo <<<ARTDOC
 <h2 class="text-center my-5">The Place</h2>
 <div class="row">
@@ -74,10 +74,22 @@ echo <<<ARTDOC
 </div>
 ARTDOC;
 
+$query = "SELECT firstName,lastName
+	  	  FROM users
+	  	  WHERE firstName = '$fname' AND lastName = '$lname'";
+$result = mysqli_query($connection,$query) or
+die("<b>Query Failed</b><br />$query<br />".mysqli_error($connection));
+
+$count = mysqli_num_rows($result);
+if($count != 0)
+{
+	
+}
+
 echo <<<ARTDOC
-<h2 class="text-center my-5">Travel</h2>
+<h2 class="text-center mt-5">Travel</h2>
 <div class="row">
-	<div class="col-md-6 embed-responsive embed-responsive-1by1" id="map">
+	<div class="col-md-5 m-5 embed-responsive embed-responsive-1by1" id="map">
 		<script>
       		function initMap() {
 				var map = new google.maps.Map(document.getElementById('map'), {
@@ -93,13 +105,16 @@ echo <<<ARTDOC
 			}
       	</script>
 	</div>
-	<div class="col-md-6">
-		<p class="text-center">We know there are many of our family members who are coming from out of town. If you are planning on staying in town</p>
+	<div class="col-md-6 my-5">
+		<p class="text-center">We know there are many of our family members who are coming from out of town. If you are planning on staying in town, we would suggest looking up and booking a hotel as soon as you possibly can. St. Patrick's park is just north of Notre Dame and the weekend of our wedding is a Notre Dame/USC game played at Notre Dame. While traffic will not be bad once you are off the highway, the hotels in the area will be full or have severely increased prices for that weekend. I would suggest looking for hotel locations in Niles, LaPorte, Plymouth, Elkhart, and Saint Saint Joseph. All of those locations are within an hour drive of the wedding location and should not be majorly impacted by the Notre Dame game.</p>
+		<p class="text-center">The following button will provide you with driving directions from the address you listed when you registered for this site to St. Patrick's Park.</p>
+		<a class="btn btn-primary col-md-6 offset-md-3 my-2" id="login" href="https://www.google.com/maps/dir/$addr,+$city,+$state+$zip,+USA/St.+Patrick's+County+Park,+50651+Laurel+Rd,+South+Bend,+IN+46637/@40.6757848,-87.5584774,8z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x886b5d95fce445cd:0xbdc9a99ac026ad58!2m2!1d-86.1499172!2d39.6083159!1m5!1m1!1s0x88112cc8b9dd73d9:0xf1f9e22772d7ac84!2m2!1d-86.2655692!2d41.7551906" role="button">Direction</a>
 	</div>
 </div>
 ARTDOC;
 
 echo "<script async defer src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyDX0uVUPKuqBLPO_Q_mmhrKD-9saPEaVoc&callback=initMap\"></script>\n";
+*/
 
 mysqli_close($connection);
 require("weddingFoot.inc");
